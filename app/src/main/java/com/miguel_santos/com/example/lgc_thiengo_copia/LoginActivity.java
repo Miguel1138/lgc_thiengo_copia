e0d58f64a15babad3c6ea3ab0157a2cc292563dc
Miguel <miguels.dasilva00@gmail.com>
2021-01-30 14:30:59 -0300

    Initial commit

---
+++ app/src/main/java/com/miguel_santos/com/example/lgc_thiengo_copia/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.miguel_santos.com.example.lgc_thiengo_copia;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        String rotuloCadastro = "Ainda não tem conta? <font color=\"#1193f5\">CADASTRE-SE</font>";
+        TextView tvCadastro = findViewById(R.id.tv_cadastro);
+        tvCadastro.setText(Html.fromHtml(rotuloCadastro));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
